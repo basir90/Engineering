4a88f268cf7d97ea042ba47807a058e6c9c4ddde
Mohammad B Mohammad Aman <mmohamm35@ccs.local>
2019-11-13 14:17:42 -0500

    mrh adding stuff

---
+++ Hello World.cpp
@@ -0,0 +1,20 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+ void setup()
+{
+lcd.begin(16, 2); 
+lcd.print("Hello World.");
+
+}
+
+ void loop()
+
+ {
+ 	lcd.setCursor(0,1);
+ }
+
+
+	
+
+
+
